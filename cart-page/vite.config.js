299e7bfa39315a68c6595dfa6c5afa12e1a19da6
lidasharova <Lida050496@yandex.ru>
2023-10-06 15:35:29 +0300

    feat: update vite configs

--- cart-page/vite.config.js
+++ cart-page/vite.config.js
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'src/[name]-[hash].js',
+        entryFileNames: 'src/[name]-[hash].js',
+
+        assetFileNames: ({ name: fileName }) => {
+          if (/\.css$/.test(fileName ?? '')) {
+            return 'styles/[name]-[hash][extname]';
+          }
+
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
+  plugins: [tsconfigPaths()],
 });
